39b40092dbdb661381adafbe42cb13134989d653
Itggot-William-Lonner <william.lonner@itggot.se>
2018-04-27 14:13:07 +0200

    2018-04-27

---
+++ site/public/js/script.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.container header .dropbtn ')) {
+
+    var dropdowns = document.getElementsByClassName(".container header dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('.container header show')) {
+        openDropdown.classList.remove('.container header show');
+      }
+    }
+  }
+}
